9552e93b7e375c635718b0a72c28412287f3e57c
Ericson Castasus <castasuse@gmail.com>
2022-03-08 10:27:56 +0800

    add user model

--- server/server.js
+++ server/server.js
@@ -3,10 +3,21 @@ import config to access the port that
 the server will listen to
 */
 import config from "./../config/config";
-/*
-import configured express file
-*/
+
+//import configured express file
 import app from "./express";
+
+//mongoose configuration
+import mongoose from "mongoose";
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoUri);
+
+mongoose.connection.on("error", () => {
+    throw new Error(`unable to connect to database: ${config.mongoUri}`);
+});
+
+
 
 app.listen(config.port, (err) => {
     if (err){
