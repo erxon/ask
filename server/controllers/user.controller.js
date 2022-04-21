575eb1b35727c161dd15eff0a506a07eea5a1d98
Ericson Castasus <castasuse@gmail.com>
2022-04-21 14:20:12 +0800

    update user profile

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model";
 import extend from "lodash/extend";
 import errorHandler from "../helpers/dbErrorHandler";
+import formidable from "formidable";
+import fs from "fs";
 
 const create = async (req, res) => {
     
@@ -17,6 +19,7 @@ const create = async (req, res) => {
         });
     }
 };
+
 const list = async (req, res) => {
     try{
         let users = await User.find().select("name email updated created");
@@ -43,26 +46,42 @@ const userByID = async (req, res, next, id) => {
         });
     }
 };
+
 const read = (req, res) => {
     req.profile.hashed_password = undefined;
     req.profile.salt = undefined;
     return res.json(req.profile);
 };
+
 const update = async (req, res) => {
-    try{
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, async (err, fields, files) =>{
+        if(err) {
+            return res.status(400).json({
+                error: "Photo could not be uploaded"
+            });
+        }
         let user = req.profile;
-        user = extend(user, req.body);
+        user = extend(user, fields);
         user.updated = Date.now();
-        await user.save();
-        user.hashed_password = undefined;
-        user.salt = undefined;
-        res.json(user);
-    } catch (err){
-        return res.status(400).json({
-            error: errorHandler.getErrorMessage(err)
-        });
-    }
+        if(files.photo) {
+            user.photo.data = fs.readFileSync(files.photo.path);
+            user.photo.contentType = files.photo.type;
+        }
+        try{
+            await user.save();
+            user.hashed_password = undefined;
+            user.salt = undefined;
+            res.json(user);
+        } catch (err){
+            return res.status(400).json({
+                error: errorHandler.getErrorMessage(err)
+            });
+        }
+    });
 };
+
 const remove = async (req, res) => {
     try{
         let user = req.profile;
@@ -77,4 +96,13 @@ const remove = async (req, res) => {
     }
 };
 
-export default { create, userByID, read, list, remove, update };
+const photo = (req, res, next) => {
+    if(req.profile.photo.data){
+    res.set("Content-Type", req.profile.photo.contentType)
+    return res.send(req.profile.photo.data)
+    }
+    next();
+};
+   
+
+export default { create, userByID, read, list, remove, update, photo };
